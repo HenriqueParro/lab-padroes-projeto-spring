2dddd6fad5a2b81db13a05dcb010aa438655b652
Henrique Parro <59770362+HenriqueParro@users.noreply.github.com>
2025-09-01 11:17:13 -0300

    Update ClienteRepository.java

--- src/main/java/one/digitalinnovation/gof/model/ClienteRepository.java
+++ src/main/java/one/digitalinnovation/gof/model/ClienteRepository.java
@@ -1,9 +1,13 @@
 package one.digitalinnovation.gof.model;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+public interface ClienteRepository extends PagingAndSortingRepository<Cliente, Long> {
+
+    Page<Cliente> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
 
 }
